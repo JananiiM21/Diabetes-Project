1e68f692bbe49cbaf88667f42e6fc92ab5bcf32c
JananiiM21 <148810288+JananiiM21@users.noreply.github.com>
2023-12-06 11:00:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask, request, jsonify
+ 
 import mysql.connector
+ 
 from flask_cors import CORS
-
+ 
 import pickle
-
+ 
 scaler = pickle.load(open('scaler.pkl', 'rb'))
 model = pickle.load(open('svm_model.pkl', 'rb'))
-
+ 
 app = Flask(__name__)
 CORS(app, origins="*", supports_credentials=True, methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"])
-
+ 
 app.config['SECRET_KEY'] = 'supersecret'
-
-
-
-
+ 
 app.config['PROPAGATE_EXCEPTIONS'] = True
-
+ 
 mysql_config = {
     'host': "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     'user': "j6qbx3bgjysst4jr",
@@ -24,16 +23,25 @@ mysql_config = {
     'database': "nkw2tiuvgv6ufu1z",
     'port': 3306,
 }
+ 
 mysql = mysql.connector.connect(**mysql_config)
-
-@app.route('/', methods=['GET', 'POST'])
+ 
+@app.route('/', methods=['GET', 'POST', 'OPTIONS'])
 def home():
+    if request.method == 'OPTIONS':
+        # Handle preflight OPTIONS request
+        response = jsonify(success=True)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        return response
+ 
     prediction = -1
+ 
     if request.method == 'POST':
         data = request.json
         cursor = mysql.cursor()
-        # mobilenumber=str(data.get('mnumber'))
-        patient_id=int(data.get('patient_id'))
+        patient_id = int(data.get('patient_id'))
         pregs = int(data.get('pregs'))
         gluc = int(data.get('gluc'))
         bp = int(data.get('bp'))
@@ -42,34 +50,34 @@ def home():
         bmi = float(data.get('bmi'))
         fun = float(data.get('fun'))
         age = int(data.get('age'))
-        
         input_features = [[pregs, gluc, bp, skin, insuli, bmi, fun, age]]
-        # print(input_features)
+ 
         prediction = model.predict(scaler.transform(input_features))
-        # print(prediction)
-
         prediction_value = prediction[0].item()
-
+ 
         insert_query = """
         INSERT INTO diabetes2 (patient_id, pregs, gluc, bp, skin, insuli, fun, result)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         """
-
+ 
         insert_data = (patient_id, pregs, gluc, bp, skin, insuli, fun, prediction_value)
-
+ 
         cursor.execute(insert_query, insert_data)
-
-        
+ 
         mysql.commit()
         cursor.close()
-
+ 
         if prediction_value == 0:
-            return jsonify(message='It is unlikely for the patient to have diabetes!', prediction_value=prediction_value)
+            response = jsonify(message='It is unlikely for the patient to have diabetes!', prediction_value=prediction_value)
         else:
-            return jsonify(message='It is highly likely that the patient already has or will have diabetes!', prediction_value=prediction_value)
-     
-        
+            response = jsonify(message='It is highly likely that the patient already has or will have diabetes!', prediction_value=prediction_value)
+ 
+        # Set CORS headers for the response
+        response.headers.add('Access-Control-Allow-Origin', '*')
+ 
+        return response
+ 
     return "Diabetes Disease Detection"
-
+ 
 if __name__ == '__main__':
     app.run(debug=True)
